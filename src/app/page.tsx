e0b72681b18d702d49b23c33e70acd389cefb15c
orphandeity <88175879+orphandeity@users.noreply.github.com>
2023-04-09 20:47:18 -0700

    homepage redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-
 export default function Home() {
-  const router = useRouter();
-  // immediately redirect to Mercury page
-  // router.push("/mercury");
-
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  );
+  return null;
 }
